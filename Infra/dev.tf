7a2a04a8ffb2465c84ab0adde0af58e639b08cd8
santyy88 <70894337+santyy88@users.noreply.github.com>
2025-09-20 18:15:02 +0530

    Delete Infra/dev.tf

--- Infra/dev.tf
+++
@@ -1,7 +0,0 @@
-subscription_id = "DEV_SUBSCRIPTION_ID"
-tenant_id       = "DEV_TENANT_ID"
-client_id       = "DEV_CLIENT_ID"
-client_secret   = "DEV_CLIENT_SECRET"
-location        = "eastus"
-aks_node_count  = 1
-
