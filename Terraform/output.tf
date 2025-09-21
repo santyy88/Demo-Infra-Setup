7be0758e0555ddd238694d1f1a12e7faca81d95e
santyy88 <70894337+santyy88@users.noreply.github.com>
2025-09-21 12:32:21 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,12 +1,17 @@
+# Output the Resource Group name
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "The name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.name
 }
 
+# Output the AKS Cluster name
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the Azure Kubernetes Service (AKS) cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
+# Output the Key Vault URI
 output "key_vault_uri" {
-  value = azurerm_key_vault.kv.vault_uri
+  description = "The URI of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.vault_uri
 }
-
