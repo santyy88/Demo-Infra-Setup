ea539962e1cd39c6e94c0954e4c07b46d12d1f35
santyy88 <70894337+santyy88@users.noreply.github.com>
2025-09-21 12:19:09 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,18 +1,65 @@
-terraform {
-  required_version = ">=1.5.0"
-
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage123"
-    container_name       = "tfstate"
-    key                  = "infra.tfstate"
-  }
+# Azure Service Principal
+variable "subscription_id" {
+  type = string
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_secret   = var.client_secret
+variable "tenant_id" {
+  type = string
 }
 
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+# Location
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+# Resource Group
+variable "resource_group_name" {
+  type = string
+}
+
+# Virtual Network
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+
+# Subnets
+variable "subnet_web" {
+  type = list(string)
+}
+
+variable "subnet_app" {
+  type = list(string)
+}
+
+variable "subnet_db" {
+  type = list(string)
+}
+
+# AKS Cluster
+variable "aks_node_count" {
+  type    = number
+  default = 2
+}
+
+variable "aks_node_size" {
+  type    = string
+  default = "Standard_DS2_v2"
+}
+
+# Key Vault
+variable "key_vault_name" {
+  type = string
+}
